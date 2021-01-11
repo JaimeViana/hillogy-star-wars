3369576b4632335455de652c6a3cebf6f483de3e
JaimeViana <jaime.viana.jimenez@gmail.com>
2021-01-11 19:22:58 +0100

    Recogemos el id de la nave a partir de la url para traer el detalle. Faltan modales.

--- src/app/ships/starship/starship.component.ts
+++ src/app/ships/starship/starship.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Ship } from '../../models/ship.model';
+import { ShipsService } from '../../app-services/ships.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-starship',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarshipComponent implements OnInit {
 
-  constructor() { }
+  ship: Ship;
+
+  constructor(
+    private shipsService: ShipsService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(async params => {
+      try {
+        this.ship = await this.shipsService.getById(params.shipId);
+      } catch (error) {
+        console.log(error);
+      }
+    });
   }
 
 }
