adbbb1e3631536de6015783d6bb960e7a452a5ca
JaimeViana <jaime.viana.jimenez@gmail.com>
2021-01-16 13:09:21 +0100

    Mejora de la navegación en componente de registro.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { User } from '../../models/user.model';
 import { UserLocalStorageService } from '../../app-services/user.local-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,10 @@ export class RegisterComponent {
 
   form: FormGroup;
 
-  constructor(public userLocalStorageService: UserLocalStorageService) {
+  constructor(
+    public userLocalStorageService: UserLocalStorageService,
+    private router: Router
+  ) {
     this.form = new FormGroup({
       firstName: new FormControl(''),
       lastName: new FormControl(''),
@@ -24,7 +28,7 @@ export class RegisterComponent {
   }
 
 
-  registerUser() {
+  register() {
     const user = new User({
       firstName: this.form.controls.firstName.value,
       lastName: this.form.controls.lastName.value,
@@ -34,6 +38,7 @@ export class RegisterComponent {
 
     if (user.comparePassword(this.form.controls.attemptedPassword.value)) {
       this.userLocalStorageService.registerUser(user);
+      this.router.navigate(['/login'])
     } else {
       console.log('Passwords do not match. Please, repeat your password.');
     }
